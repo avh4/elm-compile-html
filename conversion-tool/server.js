a08282a3b995716be1fbfe9308dfa76ee4facdc0
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-07-13 23:15:43 -0700

    Remove conversion-tool (Moved to separate git repo)

--- conversion-tool/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static('public'));
-
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-});
